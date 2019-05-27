ebfa0b0634c6f25b5dc4e09a65bdaf7026309648
ihnokim <ihnokim58@gmail.com>
2019-05-27 23:43:20 +0900

    implement legend

--- datk/visualizer.py
+++ datk/visualizer.py
@@ -1,6 +1,8 @@
 import numpy as np
+from matplotlib.lines import Line2D
 
-def put_coords(subplot, data, labels, annotate=True):
+
+def put_coords(subplot, data, labels, annotate=True, legend=True):
     data = np.array(data)
     labels = np.array(labels)
     for i, label in enumerate(set(labels)):
@@ -9,6 +11,14 @@ def put_coords(subplot, data, labels, annotate=True):
         subplot.scatter(group[:, 0], group[:, 1], label=label, s=50, alpha=0.7)
         if annotate:
             subplot.annotate(label, xy=(group[0][0], group[0][1]), xytext=(-5, 5), textcoords='offset points')
-    subplot.legend(loc='best')
+    if legend:
+        subplot.legend(loc='best')
     # subplot.set_xticks([])
     # subplot.set_yticks([])
+
+
+def legend(subplot, labels, colors):
+    lines = []
+    for color in colors:
+        lines.append(Line2D([0], [0], linewidth=7.0, linestyle='-', color=color))
+    subplot.legend(lines, labels, loc='best', borderpad=0.7)
